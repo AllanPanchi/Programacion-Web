3f8c5f953ab10f85ea34f1a4a39a0e5df1c31faf
Alejo Andrade <02alejo20@gmail.com>
2024-01-29 08:52:57 -0500

    Cambio de colores (falta login y registo)

--- Proyecto/index.php
+++ Proyecto/index.php
@@ -34,7 +34,7 @@ function esUsuario()
 <body>
     <header class="header">
         <img class="header__logo" src="Images/logo.png">
-        <h1 class="header__titulo">GameLib</h1>
+        <h1 class="header__titulo"><span>GameLib</span></h1>
     </header>
     <nav class="nav">
         <ul class="nav__ul">
@@ -47,30 +47,25 @@ function esUsuario()
     </nav>
     <section class="contenedor">
         <section class="contenedor__contenido">
-            <h2 class="contenedor__contenido-titulo">Descarga</h2>
-            <p class="contenedor__contenido-parrafo">Guarda de forma local los wallpapers de tu preferencia, o en tu
-                cuenta
-                como favoritos.</p>
-            <img class="contenedor__contenido-img" src="Images/downloads.png">
-        </section>
-        <section class="contenedor__contenido">
             <h2 class="contenedor__contenido-titulo">Conoce</h2>
-            <p class="contenedor__contenido-parrafo">Navega a travez de los menus para encontrar reseñas de los mejores
-                juegos de la actualidad y wallpapers.</p>
+            <p class="contenedor__contenido-parrafo">Navega a travez de los menus para encontrar reseñas de los mejores juegos de la actualidad.</p>
             <img class="contenedor__contenido-img" src="Images/magnifier.png">
         </section>
         <section class="contenedor__contenido">
             <h2 class="contenedor__contenido-titulo">Comparte</h2>
-            <p class="contenedor__contenido-parrafo">Comparte tus reseñas y wallpapers favoritos con tus amigos.</p>
+            <p class="contenedor__contenido-parrafo">Comparte tus reseñas favoritos con tus amigos.</p>
             <img class="contenedor__contenido-img" src="Images/share.png">
         </section>
         <section class="contenedor__contenido">
             <h2 class="contenedor__contenido-titulo">Publica</h2>
-            <p class="contenedor__contenido-parrafo">Publica tus reseñas y wallpapers favoritos para que otros usuarios
+            <p class="contenedor__contenido-parrafo">Publica tus reseñas favoritos para que otros usuarios
                 puedan verlos.</p>
             <img class="contenedor__contenido-img" src="Images/upload.png">
         </section>
     </section>
+    <aside class="aside">
+        <p class="aside__eslogan">Proyecta tu filosofia</p>
+    </aside>
     <footer class="footer">
         <p class="footer__copyright">&copy GameLib 2023</p>
     </footer>
