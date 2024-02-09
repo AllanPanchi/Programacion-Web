a90fdfcd4b62b776b3a0d6042298236aed29572b
Alejo Andrade <02alejo20@gmail.com>
2024-02-09 02:33:00 -0500

    index - GameLib

--- GameLib/scripts/main.js
+++ GameLib/scripts/main.js
@@ -5,17 +5,9 @@ function bloquearScroll() {
 
     // Guarda el ancho actual del cuerpo para mantenerlo fijo
     var bodyWidth = document.body.clientWidth;
-
-    // Agrega padding-right para compensar el ancho de la barra de desplazamiento
     document.body.style.paddingRight = scrollBarWidth + 'px';
-
-    // Evita que el cuerpo se desplace
     document.body.style.overflow = 'hidden';
-
-    // Fija el cuerpo en su posición actual
     document.body.style.position = 'fixed';
-
-    // Ajusta el ancho del cuerpo para evitar que se ajuste al eliminar la barra de desplazamiento
     document.body.style.width = bodyWidth + 'px';
 }
 
@@ -56,9 +48,35 @@ document.getElementById('btn-close-logIn').addEventListener('click', function ()
 
 document.getElementById('form-LogIn').addEventListener('submit', function (event) {
     event.preventDefault();
-    
-    localStorage.setItem('user', document.getElementById('user').value);
-    localStorage.setItem('pass', document.getElementById('pass').value);
+    if (document.getElementById('user').value == 'user' && document.getElementById('pass').value == 'user') {
+        localStorage.setItem('user', document.getElementById('user').value);
+        localStorage.setItem('pass', document.getElementById('pass').value);
+        window.location.href = 'pages/juegos.html';
+    } else {
+        alert('Usuario o contraseña incorrectos');
+    }
 
-    window.location.href = 'pages/juegos.html';
+});
+
+// funcion para cambiar el background de un elemento de una lista de colores que se selecciona aleatoriamente
+function changeColor(id) {
+    var colors = ['#38725b', '#4b5dab', '#a4587b', '#d8b24a', '#826dbf', '#d8805e', '#4e9791', '#8c8c8c', '#e0a48f', '#81b7cf'];
+    var randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.getElementById(id).style.backgroundColor = randomColor;
+}
+
+changeColor('conoce-bg');
+changeColor('comparte-bg');
+changeColor('publica-bg');
+
+document.getElementById('conoce-bg').addEventListener('mouseover', function () {
+    changeColor('conoce-bg');
+});
+
+document.getElementById('comparte-bg').addEventListener('mouseover', function () {
+    changeColor('comparte-bg');
+});
+
+document.getElementById('publica-bg').addEventListener('mouseover', function () {
+    changeColor('publica-bg');
 });
